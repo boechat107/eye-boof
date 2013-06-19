d65405a0be9758ce39b73ee57a2328cdf85dd2ce
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-06-19 17:39:01 -0300

    Manually image file loading; there is bug w or r an image
    * Perhaps it is a good idea to write my own to-bufferedimage
    function.

--- src/eye_boof/helpers.clj
+++ src/eye_boof/helpers.clj
@@ -8,7 +8,6 @@
     [java.io File]
     [javax.imageio ImageIO]
     [java.awt.image BufferedImage]
-    [boofcv.io.image UtilImageIO]
     [boofcv.struct.image ImageBase ImageUInt8 MultiSpectral]
     [boofcv.core.image ConvertBufferedImage]
     )
@@ -55,22 +54,42 @@
 
 (defn load-file-image
   "Returns a RGB Image from a file image."
-  ;; Try and catch.
   [^String filepath]
-  (let [buff (UtilImageIO/loadImage filepath)
-        img (ConvertBufferedImage/convertFromMulti buff nil ImageUInt8)]
-    (ConvertBufferedImage/orderBandsIntoRGB img buff)
-    (c/make-image img :rgb)))
+  (let [buff (ImageIO/read (File. filepath))
+        nc (.getWidth buff)
+        img (c/new-image (.getHeight buff) nc :rgb)
+        rch (c/get-channel img 0)
+        gch (c/get-channel img 1)
+        bch (c/get-channel img 2)]
+;    (assert (== BufferedImage/TYPE_4BYTE_ABGR (.getType buff)))
+    (c/for-xy
+        [x y img]
+        (let [pix (.getRGB buff x y)
+              i (+ x (* y nc))]
+          (c/set-pixel! rch i (r<-intcolor pix))
+          (c/set-pixel! gch i (g<-intcolor pix))
+          (c/set-pixel! bch i (b<-intcolor pix))
+          )
+        )
+    img
+    ))
 
 (defn to-buffered-image
   "Converts an ARGB Image to a BufferedImage."
-  ;; The resultant image is not good, perhaps because of the channels ordering.
-  [img]
+  ;; fixme: The resultant image is not good, perhaps because of the channels ordering.
+  ^BufferedImage [img]
   {:pre [(c/image? img)]}
   (let [b ^ImageBase (:mat img)]
     (if (> (c/dimension img) 1)
       (ConvertBufferedImage/convertTo_U8 b nil)
       (ConvertBufferedImage/convertTo b nil))))
+
+(defn save-to-file!
+  "Saves an image into a file."
+  ([img filepath] (save-to-file! img filepath "png"))
+  ([img ^String filepath ^String ext]
+   (-> (to-buffered-image img)
+       (ImageIO/write ext (File. filepath)))))
 
 (defn view 
   "Shows the images on a grid-panel window."
--- test/eye_boof/core_test.clj
+++ test/eye_boof/core_test.clj
@@ -3,7 +3,47 @@
   (:require 
     [eye-boof.core :as c]
     [eye-boof.helpers :as h]
-    [eye-boof.processing :as p]))
+    [eye-boof.processing :as p])
+  (:import 
+    [boofcv.struct.image ImageBase ImageUInt8 ImageSInt16 ImageFloat32 MultiSpectral]  
+    )
+  )
+
+(def img-test 
+  "A simple image created with imgpviewer. It is 4x4 image with one white pixel, one
+  red, one green and one blue, the others are all black."
+  ;(h/load-file-image "test/img_test.png")
+  (let [img (c/new-image 4 4 :rgb)
+        rch (c/get-channel img 0)
+        gch (c/get-channel img 1)
+        bch (c/get-channel img 2)
+        fname "test/img_boofcv.png"]
+    (c/set-pixel! rch 0 255)
+    (c/set-pixel! gch 0 255)
+    (c/set-pixel! bch 0 255)
+    (c/set-pixel! rch 5 255)
+    (c/set-pixel! gch 10 255)
+    (c/set-pixel! bch 15 255)
+    img
+;    (h/save-to-file! img fname)
+;    (h/load-file-image fname)
+    ))
+
+(deftest pixel-vals
+  (let [rch (c/get-channel img-test 0)
+        gch (c/get-channel img-test 1)
+        bch (c/get-channel img-test 2)]
+    (is (== 255 (c/get-pixel rch 0)))
+    (is (== 255 (c/get-pixel gch 0)))
+    (is (== 255 (c/get-pixel bch 0)))
+    (is (== 255 (c/get-pixel rch 5)))
+    (is (== 0 (c/get-pixel gch 5)))
+    (is (== 0 (c/get-pixel bch 5)))
+    (is (== 0 (c/get-pixel rch 10)))
+    (is (== 255 (c/get-pixel gch 10)))
+    (is (== 0 (c/get-pixel bch 10)))
+    )
+  )
 
 (defn time-test
   []
@@ -12,7 +52,7 @@
         bin (time (p/binarize gray 100))
         blur (time (p/mean-blur img 1))
         ]
-    ;(time (h/view img blur))
+    (time (h/view img blur))
     nil
     )
   )
