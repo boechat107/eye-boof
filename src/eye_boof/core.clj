eda11ef22d1180e9e80a88a26c8223df94d7e459
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-07-06 19:34:02 -0300

    Forgotten quote

--- src/eye_boof/core.clj
+++ src/eye_boof/core.clj
@@ -198,7 +198,7 @@
    `(let [ch# ~(vary-meta ch assoc :tag 'boofcv.struct.image.ImageUInt8)]
       (mult-aset ~'bytes (.data ch#) ~idx (unchecked-byte ~val))))
   ([ch x y val]
-   (set-pixel! ~ch (+ (.startIndex ~ch) (+ (* ~y (.stride ~ch)) ~x)) ~val)))
+   `(set-pixel! ~ch (+ (.startIndex ~ch) (+ (* ~y (.stride ~ch)) ~x)) ~val)))
 
 (defn set-pixel!*
   "Sets the value of the [x, y] pixel for a given channel."
