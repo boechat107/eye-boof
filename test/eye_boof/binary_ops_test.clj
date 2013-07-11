434b267b12cd2bfdea802bb31b495ce67b31062d
Vinicius@dell <vinicius.darocha@das.ufsc.br>
2013-07-11 11:34:14 -0300

    Added test for labeled-img functions and for contours.

--- test/eye_boof/binary_ops_test.clj
+++ test/eye_boof/binary_ops_test.clj
@@ -8,10 +8,31 @@
   (:import [georegression.struct.point Point2D_I32]
            [boofcv.struct.image ImageSInt32]))
 
-(deftest contour-test
+(deftest labeled-img-test
   (is (= (-> (binop/labeled-image img-small-connected 4) .data vec)
          [0 0 0 0 1 1 1 2 2 2 0 1 1 1 2 0 2 0 1 1 0 2 2 2 0 0 0 0 0 0 0 0 3 3 0 0 4 4 0 3 3 0 0 4 4 0 0 0 5])
       "Labeled image with 4-neighbours")
+  ;;Perhaps this test is reduntant, but test it anyway =)
   (is (= (-> (binop/labeled-image img-small-connected 8) .data vec)
          [0 0 0 0 1 1 1 2 2 2 0 1 1 1 2 0 2 0 1 1 0 2 2 2 0 0 0 0 0 0 0 0 3 3 0 0 4 4 0 3 3 0 0 4 4 0 0 0 3])
       "Labeled image with 8-neighbours"))
+
+(deftest contour-test
+  (let [contours4 (binop/contour img-small-connected 4)]
+    (is (= 5 (count contours4))
+        "Contours4: correct amount of contours")
+    (is (= 1 (count (keep #(-> % .internal seq) contours4)))
+        "Contours4: correct amount of internal contours")
+    (is (= 5 (count (keep #(-> % .external seq) contours4)))
+        "Contours4: correct amount of external contours"))
+  
+  (let [contours8 (binop/contour img-small-connected 8)]
+    (is (= 4 (count contours8))
+        "Contours8: correct amount of contours")
+    (is (= 1 (count (keep #(-> % .internal seq) contours8)))
+        "Contours8: correct amount of internal contours")
+    ;;Perhaps this test is reduntant, but test it anyway =)
+    (is (= 4 (count (keep #(-> % .external seq) contours8)))
+        "Contours8: correct amount of external contours")))
+
+
