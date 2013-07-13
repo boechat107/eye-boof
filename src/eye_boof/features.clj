40882d8d22d9cc0438de6427750b8198ed1ebcc4
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-07-13 10:32:32 -0300

    Bounding box area calculation; macros to avoid reflections

--- src/eye_boof/features.clj
+++ src/eye_boof/features.clj
@@ -5,10 +5,26 @@
     [boofcv.alg.feature.shapes ShapeFittingOps]
     [georegression.struct.point Point2D_I32]))
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
+
+(defmacro pt-getter
+  [pt xy]
+  `(let [pt# ~(vary-meta pt assoc :tag 'georegression.struct.point.Point2D_I32)]
+     (. pt# ~xy)))
+
+(defmacro x
+  [pt]
+  `(pt-getter ~pt x))
+
+(defmacro y 
+  [pt]
+  `(pt-getter ~pt y))
+
 (defn pts-to-vec
   "Returns a sequence of vectors [x y] from the given list of Points."
   [pts]
-  (map #(vector (.x %) (.y %)) pts))
+  (map #(vector (x %) (y %)) pts))
 
 (defn fit-polygon
   "Returns a list of points PointIndex_I32 representing the vertexes of a fitted
@@ -27,7 +43,15 @@
   given list of points."
   [pts]
   (when (> (count pts) 2)
-    (let [sy (sort-by #(.y %) pts)
-          sx (sort-by #(.x %) pts)]
-      [(Point2D_I32. (.x (first sx)) (.y (first sy)))
-       (Point2D_I32. (.x (last sx)) (.y (last sy)))])))
+    (let [sy (sort-by #(y %) pts)
+          sx (sort-by #(x %) pts)]
+      [(Point2D_I32. (x (first sx)) (y (first sy)))
+       (Point2D_I32. (x (last sx)) (y (last sy)))])))
+
+(defn aprox-area
+  "Returns the area of a bounding box around the given list of Points."
+  [pts]
+  (when (> (count pts) 2)
+    (let [[tl br] (bounding-box pts)]
+      (* (- (x br) (x tl))
+         (- (y br) (y tl))))))
