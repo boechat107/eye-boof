45ff19b4b05d290a368baad70e24420e56d1d451
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-07-22 12:54:36 -0300

    Option to specify the channel whose mean is calculated

--- src/eye_boof/image_statistics.clj
+++ src/eye_boof/image_statistics.clj
@@ -1,6 +1,8 @@
 (ns eye-boof.image-statistics
-  (:require [eye-boof.core :as eyec])
-  (:import [boofcv.alg.misc ImageStatistics]))
+  (:require [eye-boof.core :as c])
+  (:import 
+    [boofcv.alg.misc ImageStatistics]
+    [boofcv.struct.image ImageUInt8 ImageSInt16 ImageFloat32]))
 
 
 ;;(TODO) implement the remaining functions
@@ -15,11 +17,11 @@
 ;; histogram
 
 (defn mean
-  "Returns the mean pixel intensity"
-  [img]
-  (if (eyec/one-dim? img)
-    (ImageStatistics/mean (eyec/get-channel img))
-    (throw (Exception. "Not implemented yet"))))
+  "Returns the mean pixel intensity of the channel ch (default 0) of the given
+  image."
+  ([img] (mean img 0))
+  ([img ch]
+   (ImageStatistics/mean (c/get-channel img ch))))
 
 
 ;; vertical-histogram
