84c149d631ddac6ad8e06b9676e2a2933e9374f0
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-07-24 16:47:15 -0300

    Generalization of the matrices namespace functionality
    * The core functions get-pixel and set-pixel! can be implemented
    based on the functions of matrices namespace.

--- src/eye_boof/matrices.clj
+++ src/eye_boof/matrices.clj
@@ -2,7 +2,8 @@
   (:require [eye-boof.core :as eyec])
   (:import [boofcv.struct.image ImageSInt8 ImageSInt16 ImageSInt32 ImageSInt64]))
 
-
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
 
 (def image-data-type
   {'ImageSInt8  bytes
@@ -12,37 +13,46 @@
    'ImageUInt8  bytes
    'ImageUInt16 shorts})
 
-(defmacro get-pixel
-  "Returns a primitive integer value from a channel's array ach. If coordinates 
-  [x, y] and ncols are provided, the array is handled as 2D matrix.
-  Warning: idx is relative to the original or parent image, so it is dangerous to use it
-  for sub-images, give preference to x and y indexing.
+(defmacro mget
+  "Returns a primitive integer value from a matrix's array mat. If coordinates [x, y],
+  the array is handled as 2D matrix.
+  Warning: idx is relative to the original or parent image, so it is dangerous to use
+  it for sub-images, give preference to x and y indexing.
   e.g.
     => (get-pixel ImageSInt8 img 2)"
-  ([type ch idx]
+  ([type mat idx]
      (if-let [internal-type (get image-data-type type)]
-       `(let [ch# ~(vary-meta ch assoc :tag type)]
-          (eyec/mult-aget ~internal-type (.data ch#) ~idx))
+       `(let [mat# ~(vary-meta mat assoc :tag type)]
+          (eyec/mult-aget ~internal-type (.data mat#) ~idx))
        (throw (Exception. (str "Type" type " not recognized")))))
-  ([type ch x y]
+  ([type mat x y]
      (if-let [internal-type (get image-data-type type)]
-       `(let [ch# ~(vary-meta ch assoc :tag type)]
-          (eyec/mult-aget ~internal-type (.data ch#) (+ (.startIndex ch#) (+ ~x (* ~y (.stride ch#))))))
+       `(let [mat# ~(vary-meta mat assoc :tag type)]
+          (eyec/mult-aget ~internal-type (.data mat#) (+ (.startIndex mat#) (+ ~x (* ~y (.stride mat#))))))
        (throw (Exception. (str "Type " type "not recognized"))))))
 
+;; For compatibility.
+(defmacro get-pixel
+  ([type mat idx] `(mget ~type ~mat ~idx))
+  ([type mat x y] `(mget ~type ~mat ~x ~y)))
 
-(defmacro set-pixel! 
-  "Sets the value of a pixel for a given channel's array. If coordinates [x, y] and
-  ncols are provided, the array is handled as 2D matrix.
+(defmacro mset! 
+  "Sets the value of a matrix's element. If coordinates [x, y] are provided, the
+  array is handled as 2D matrix.
    e.g.
       (set-pixel! ImageSInt8 asd 2 2 4)"
-  ([type ch idx val]
+  ([type mat idx val]
      (if-let [internal-type (get image-data-type type)]
-       `(let [ch# ~(vary-meta ch assoc :tag type)]
-          (eyec/mult-aset ~internal-type (.data ch#) ~idx (unchecked-byte ~val)))
+       `(let [mat# ~(vary-meta mat assoc :tag type)]
+          (eyec/mult-aset ~internal-type (.data mat#) ~idx (unchecked-byte ~val)))
        (throw (Exception. (str "Type " type "not recognized")))))
-  ([type ch x y val]
+  ([type mat x y val]
      (if-let [internal-type (get image-data-type type)]
-       `(let [ch# ~(vary-meta ch assoc :tag type)]
-          (eyec/mult-aset ~internal-type (.data ch#) (+ (.startIndex ~ch) (+ (* ~y (.stride ~ch)) ~x)) (unchecked-byte ~val)))
+       `(let [mat# ~(vary-meta mat assoc :tag type)]
+          (eyec/mult-aset ~internal-type (.data mat#) (+ (.startIndex ~mat) (+ (* ~y (.stride ~mat)) ~x)) (unchecked-byte ~val)))
        (throw (Exception. (str "Type " type "not recognized"))))))
+
+;; For compatibility.
+(defmacro set-pixel!
+  ([type mat idx val] `(mset! ~type ~mat ~idx ~val))
+  ([type mat x y val] `(mset! ~type ~mat ~x ~y ~val)))
