7aab774cde8884af75e7628c81cafcf9e97ecf75
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-03-27 16:39:22 -0300

    tests for the basic fns

--- test/eye_boof/core_test.clj
+++ test/eye_boof/core_test.clj
@@ -1,96 +1,37 @@
 (ns eye-boof.core-test
   (:use clojure.test)
   (:require 
-    [eye-boof.core :as c]
-    [eye-boof.helpers :as h]
-    [eye-boof.processing :as p]))
+    [clojure.test :refer :all]
+    [eye-boof.core :refer :all]))
 
+(deftest single-band
+  (let [gray-img (new-image 4 3)]
+    (testing "Grayscale images"
+      (is (image? gray-img))
+      (is (== (nbands gray-img) 1))
+      (is (== (width gray-img) 4))
+      (is (== (height gray-img) 3))
+      (is (not (sub-image? gray-img)))
+      (set-pixel! gray-img 2 2 11)
+      (is (== (pixel gray-img 2 2) 11))
+      (let [sub-gray (sub-image gray-img 1 1 2 2)
+            [ox oy] (parent-origin sub-gray)]
+        (is (== (width sub-gray) 2))
+        (is (== (height sub-gray) 2))
+        (is (sub-image? sub-gray))
+        (is (= [ox oy] [1 1]))
+        (is (== (pixel sub-gray 1 1) 11))
+        (testing "Shared data between an image and its sub-image"
+          (set-pixel! sub-gray 0 0 4)
+          (is (== (pixel gray-img ox oy) 4)))))))
 
-(def img-test 
-  "A simple image created with imgpviewer. It is 4x4 image with one white pixel, one
-  red, one green and one blue, the others are all black."
-  ;(h/load-file-image "test/img_test.png") ; argb image
-  (let [img (c/new-image 4 4 :rgb)
-        rch (c/get-channel img 0)
-        gch (c/get-channel img 1)
-        bch (c/get-channel img 2)
-        fname "test/img_boofcv.png"]
-    (c/set-pixel! rch 0 255)
-    (c/set-pixel! gch 0 255)
-    (c/set-pixel! bch 0 255)
-    (c/set-pixel! rch 1 1 255)
-    (c/set-pixel! gch 10 255)
-    (c/set-pixel! bch 15 255)
-    (h/save-to-file! img fname)
-    (h/load-file-image fname)))
-
-(deftest pixel-vals
-  (let [rch (c/get-channel img-test 0)
-        gch (c/get-channel img-test 1)
-        bch (c/get-channel img-test 2)]
-    (is (== 255 (c/get-pixel rch 0)))
-    (is (== 255 (c/get-pixel gch 0)))
-    (is (== 255 (c/get-pixel bch 0)))
-    (is (== 255 (c/get-pixel rch 5)))
-    (is (== 0 (c/get-pixel gch 5)))
-    (is (== 0 (c/get-pixel bch 5)))
-    (is (== 0 (c/get-pixel rch 10)))
-    (is (== 255 (c/get-pixel gch 10)))
-    (is (== 0 (c/get-pixel bch 10)))
-    (is (= (c/channel-to-vec img-test 0)
-           [255 0 0 0 0 255 0 0 0 0 0 0 0 0 0 0]))
-    (is (= (c/channel-to-vec img-test 1)
-           [255 0 0 0 0 0 0 0 0 0 255 0 0 0 0 0]))
-    (is (= (c/channel-to-vec img-test 2)
-           [255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255]))))
-
-(deftest sub-images
-  (let [w 3
-        h 3
-        si (c/sub-image img-test 0 0 h w)
-        [rch gch bch] (c/get-channel si)
-        ssi (c/sub-image si 1 1 2 2)
-        [r g b] (c/get-channel ssi)]
-    (is (== w (c/ncols si)))
-    (is (== h (c/nrows si)))
-    (is (== 255 (c/get-pixel rch 1 1)))
-    (is (== 255 (c/get-pixel gch 2 2)))
-    (is (= [0 0] (c/get-parent-point si)))
-    (is (== 2 (c/ncols ssi)))
-    (is (== 2 (c/nrows ssi)))
-    (is (== 255 (c/get-pixel r 0 0)))
-    (is (== 255 (c/get-pixel g 1 1)))
-    (is (== 0 (c/get-pixel b 1 1)))
-    (is (= [1 1] (c/get-parent-point ssi)))))
-
-(deftest img-scaling
-  (let [orig (h/load-file-image "resources/boofcv.jpg")
-        bigger (time (p/scale orig 1.5 1.5))
-        smaller (time (p/scale orig 0.5 0.5))]
-    #_(h/view* orig bigger smaller)))
-
-(defn time-set-pixel
-  []
-  (let [ch (c/new-channel-matrix 1000 1000 1)]
-    (time (dotimes [y 1000]
-            (dotimes [x 1000]
-              (c/set-pixel!* ch x y 10))))
-    (time (dotimes [y 1000]
-            (dotimes [x 1000]
-              (c/set-pixel! ch x y 10))))
-    (time (dotimes [idx 1000000]
-            (c/set-pixel!* ch idx 10)))
-    (time (dotimes [idx 1000000]
-            (aset-byte (.data ch) idx 10)))))
-
-(defn time-test
-  []
-  (let [img (time (h/load-file-image "test/cnh.png"))
-        gray (time (p/rgb-to-gray img))
-        bin (time (p/binarize gray 100))
-        blur (time (p/mean-blur img 1))
-        ]
-    #_(time (h/view img blur))
-    nil
-    )
-  )
+(deftest multispectral 
+  (let [color-img (new-image 4 3 3)]
+    (testing "Colored images"
+      (is (image? color-img))
+      (is (== (nbands color-img) 3))
+      (is (== (width color-img) 4))
+      (is (== (height color-img) 3))
+      (is (not (sub-image? color-img)))
+      (set-pixel! color-img 2 2 11 12 13)
+      (is (= (pixel color-img 2 2) [11 12 13])))))
