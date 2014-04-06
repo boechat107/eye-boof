188204383abdef03dd3f581425106278d010e91b
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-04-06 17:16:45 -0300

    No more snapshots (I think...)

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject org.clojars.boechat107/eye-boof "1.0.0-SNAPSHOT"
+(defproject org.clojars.boechat107/eye-boof "1.0.0"
   :description "Clojure image processing library using BoofCV."
   :url "https://github.com/boechat107/eye-boof"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.boofcv/ip "0.16"]
-                 [seesaw "1.4.3"]]
-  )
+                 [seesaw "1.4.3"]])
