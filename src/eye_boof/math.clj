6337ad119ec92892900507e0c03ed270f2a61cdf
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-04-06 16:52:06 -0300

    Simple redesign of scalar multiplication on images

--- src/eye_boof/math.clj
+++ src/eye_boof/math.clj
@@ -1,23 +1,18 @@
 (ns eye-boof.math
+  "Functions to apply arithmetical operations on images."
+  (:refer-clojure :exclude [*])
   (:require 
-    [eye-boof.core :as c]
-    )
+    [eye-boof.core :refer [width height new-image]])
   (:import 
     [boofcv.alg.misc PixelMath]
-    [boofcv.struct.image ImageUInt8 ImageSInt16 ImageFloat32]))
+    [boofcv.struct.image ImageUInt8]))
 
-(defmacro def-noargs-function
-  [fname doc f]
-  `(defn ~fname 
-     ~doc
-     ([~'img] (~fname ~'img 0))
-     ([~'img ~'ch]
-      (let [och# (c/new-channel-matrix (c/nrows ~'img) (c/ncols ~'img) 1)]
-        (~f (c/get-channel ~'img ~'ch) och#)
-        (c/make-image och# (:type ~'img))))))
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
 
-(def-noargs-function 
-  abs
-  "Returns a new image where its pixels' values correspond to the absolute values
-  of the given image."
-  PixelMath/abs)
+(defn *
+  "Multiplies each pixel value by a scalar value."
+  [^ImageUInt8 img v]
+  (let [^ImageUInt8 output (new-image (width img) (height img))]
+    (PixelMath/multiply img (double v) output)
+    output))
