a90640c7fa0de98b08701268cda61d5a16163e6c
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-04-27 19:36:02 -0300

    Divide pixel operation

--- src/eye_boof/math.clj
+++ src/eye_boof/math.clj
@@ -1,6 +1,6 @@
 (ns eye-boof.math
   "Functions to apply arithmetical operations on images."
-  (:refer-clojure :exclude [*])
+  (:refer-clojure :exclude [* /])
   (:require 
     [eye-boof.core :refer [width height new-image]])
   (:import 
@@ -12,7 +12,14 @@
 
 (defn *
   "Multiplies each pixel value by a scalar value."
-  [^ImageUInt8 img v]
+  [^ImageUInt8 img ^double v]
   (let [^ImageUInt8 output (new-image (width img) (height img))]
-    (PixelMath/multiply img (double v) output)
+    (PixelMath/multiply img v output)
+    output))
+
+(defn /
+  "Divides each pixel value by a scalar."
+  [^ImageUInt8 img ^double v]
+  (let [^ImageUInt8 output (new-image (width img) (height img))]
+    (PixelMath/divide img v output)
     output))
