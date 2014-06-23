601572e06c1e08c5b86e647eabc939dde975eeb1
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-06-23 13:19:02 -0300

    Finishing the snapshot of version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.boechat107/eye-boof "0.1.0-SNAPSHOT"
+(defproject eye-boof/eye-boof "0.1.0"
   :description "Clojure image processing library using BoofCV."
   :url "https://github.com/boechat107/eye-boof"
   :license {:name "Eclipse Public License"
