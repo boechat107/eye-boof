316afedba6f2ba02e0ed8d5fc35281404c9c0260
Andre Ambrosio Boechat <boechat107@gmail.com>
2016-05-16 13:35:53 +0200

    Updating Clojure's version

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "https://github.com/boechat107/eye-boof"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.boofcv/ip "0.16"]
+  :dependencies [[org.boofcv/ip "0.16"]
                  [seesaw "1.4.3"]
                  [net.mikera/clojure-utils "0.5.0"]
                  [prismatic/hiphip "0.2.0"]
                  [org.clojure/algo.generic "0.1.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
+                   :plugins [[lein-codox "0.9.4"]]}}
   :java-source-paths ["java"])
