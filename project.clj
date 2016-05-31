a5cef8c267a38f1192fc512011c18eda23f58fc1
Andre Ambrosio Boechat <boechat107@gmail.com>
2016-05-31 19:08:34 +0200

    Adding clojure 1.7.0 as a dependency again

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/boechat107/eye-boof"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.boofcv/ip "0.16"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.boofcv/ip "0.16"]
                  [seesaw "1.4.3"]
                  [net.mikera/clojure-utils "0.5.0"]
                  [prismatic/hiphip "0.2.0"]
                  [org.clojure/algo.generic "0.1.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
-                   :plugins [[lein-codox "0.9.4"]]}}
+  :profiles {:dev {:plugins [[lein-codox "0.9.4"]]}}
   :java-source-paths ["java"]
   :aliases {"test" ["test" ":only" "eye-boof.test.io"]})
