f2b6128afe5a646c1319a1705179a61e98427779
Andre Ambrosio Boechat <boechat107@gmail.com>
2017-03-09 20:08:45 -0300

    Updating project's dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,10 @@
-(defproject eye-boof/eye-boof "1.2.0-SNAPSHOT"
+(defproject eye-boof/eye-boof "2.0.0-SNAPSHOT"
   :description "Clojure image processing library using BoofCV."
   :url "https://github.com/boechat107/eye-boof"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.boofcv/ip "0.16"]
-                 [seesaw "1.4.3"]
-                 [net.mikera/clojure-utils "0.5.0"]
-                 [prismatic/hiphip "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.boofcv/core "0.26"]
+                 [prismatic/hiphip "0.2.1"]
                  [org.clojure/algo.generic "0.1.2"]]
-  :profiles {:dev {:plugins [[lein-codox "0.9.4"]]}}
-  :java-source-paths ["java"]
-  :aliases {"test" ["test" ":only" "eye-boof.test.io"]})
+  :profiles {:dev {:plugins [[lein-codox "0.10.3"]]}})
